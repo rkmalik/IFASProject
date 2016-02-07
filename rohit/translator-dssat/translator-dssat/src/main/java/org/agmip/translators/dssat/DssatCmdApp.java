7fd6c4e9fcb3b35c5c38db39d62b3fbe392e47af
Rohit Kumar Malik <rkmalik@ufl.edu>
2016-02-07 18:43:00 -0500

    Merged the Projects

---
+++ rohit/translator-dssat/translator-dssat/src/main/java/org/agmip/translators/dssat/DssatCmdApp.java
@@ -0,0 +1,114 @@
+package org.agmip.translators.dssat;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.agmip.acmo.util.AcmoUtil;
+import org.agmip.functions.DataCombinationHelper;
+import org.agmip.util.JSONAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Meng Zhang
+ */
+public class DssatCmdApp {
+
+    private static boolean isCompressed;
+    private static boolean isToModel;
+    private static ArrayList<String> inputPaths;
+    private static String outputPath;
+    private static final Logger LOG = LoggerFactory.getLogger(DssatCmdApp.class);
+
+    public static void main(String... args) throws IOException {
+        init();
+        readCommand(args);
+        if (inputPaths == null) {
+            LOG.error("There is no valid input path, please check input arguments");
+            return;
+        }
+        if (isToModel) {
+            LOG.info("Translate {} to DSSAT...", inputPaths);
+            DssatControllerOutput translator = new DssatControllerOutput();
+            HashMap data = readJson();
+            translator.writeFile(outputPath, data);
+            if (isCompressed) {
+                translator.createZip(new File(outputPath + File.separator + "DSSAT_Input.zip"));
+            }
+            writeAcmo(data);
+        } else {
+            LOG.info("Translate {} to JSON...", inputPaths.get(0));
+            DssatControllerInput translator = new DssatControllerInput();
+            Map result = translator.readFile(inputPaths.get(0));
+            BufferedOutputStream bo;
+            outputPath += File.separator;
+            File f = new File(outputPath + new File(outputPath).getName().replaceAll("\\.\\w+$", ".json"));
+            bo = new BufferedOutputStream(new FileOutputStream(f));
+
+            // Output json for reading
+            bo.write(JSONAdapter.toJSON(result).getBytes());
+            bo.flush();
+            bo.close();
+        }
+        LOG.info("Job done!");
+    }
+
+    private static void init() {
+        isCompressed = false;
+        isToModel = false;
+        inputPaths = new ArrayList();
+        outputPath = getOutputPath("");
+    }
+
+    private static void readCommand(String[] args) {
+
+        for (String arg : args) {
+            if (arg.equalsIgnoreCase("-zip")) {
+                isCompressed = true;
+            } else if (arg.toUpperCase().endsWith(".JSON")) {
+                isToModel = true;
+                inputPaths.add(arg);
+            } else if (arg.toUpperCase().endsWith(".ZIP")) {
+                isToModel = false;
+                inputPaths.add(arg);
+            } else {
+                outputPath = getOutputPath(arg);
+            }
+        }
+        LOG.info("Read from {}", inputPaths);
+        LOG.info("Output to {}", outputPath);
+    }
+
+    private static HashMap readJson() throws IOException {
+        HashMap data = DataCombinationHelper.combine(inputPaths);
+        DataCombinationHelper.fixData(data);
+        return data;
+    }
+
+    private static String getOutputPath(String arg) {
+
+        String path;
+        if (arg != null && !"".equals(arg)) {
+            path = new File(arg).getPath() + File.separator + "DSSAT";
+        } else {
+            path = "DSSAT";
+        }
+
+        File dir = new File(path);
+        int cnt = 1;
+        while (dir.exists() && dir.list().length > 0) {
+            dir = new File(path + "-" + cnt);
+            cnt++;
+        }
+        return dir.getPath();
+    }
+
+    private static void writeAcmo(HashMap data) {
+        AcmoUtil.writeAcmo(outputPath, data, "dssat", new HashMap());
+    }
+}
---
+++ rohit/translator-dssat/translator-dssat/src/test/java/org/agmip/translators/dssat/DssatXFileTest.java
@@ -0,0 +1,66 @@
+ package org.agmip.translators.dssat;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.agmip.util.JSONAdapter;
+import static org.agmip.util.MapUtil.getObjectOr;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Unit test for simple App.
+ */
+/**
+ *
+ * @author Meng Zhang
+ */
+public class DssatXFileTest {
+
+    private static final Logger log = LoggerFactory.getLogger(DssatXFileTest.class);
+    DssatXFileOutput obDssatXFileOutput;
+    DssatXFileInput obDssatXFileInput;
+    URL resource;
+
+    @Before
+    public void setUp() throws Exception {
+        obDssatXFileOutput = new DssatXFileOutput();
+        obDssatXFileInput = new DssatXFileInput();
+        resource = this.getClass().getResource("/UFGA8201_MZX.zip");
+    }
+
+    @Test
+    public void test() throws IOException, Exception {
+        HashMap result;
+
+        log.debug(resource.toString());
+
+        result = obDssatXFileInput.readFile(resource.getPath());
+
+        // Output Json text for debugging
+//        File f = new File("outputX.txt");
+//        BufferedOutputStream bo = new BufferedOutputStream(new FileOutputStream(f));
+//        bo.write(JSONAdapter.toJSON(result).getBytes());
+//        bo.close();
+//        f.delete();
+
+        ArrayList<HashMap> expArr = getObjectOr(result, "experiments", new ArrayList());
+        JSONAdapter.toJSON(result);
+        obDssatXFileOutput.writeFile("output", JSONAdapter.fromJSONFile(""));
+        obDssatXFileOutput.writeFile("output", expArr.get(0));
+        File file = obDssatXFileOutput.getOutputFile();
+        if (file != null) {
+            assertTrue(file.exists());
+            assertEquals("UFGA8201.MZX", file.getName());
+            assertTrue(file.delete());
+        } else {
+            assertTrue(file != null);
+        }
+    }
+}
